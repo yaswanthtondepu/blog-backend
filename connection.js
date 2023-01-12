23e3363ce5382bfa61fea5d7c51e7197cd88b535
yash1744 <yashwanthboyapally@gmail.com>
2023-01-12 17:24:58 -0600

    created backend for signup

---
+++ connection.js
@@ -0,0 +1,11 @@
+const mysql = require("mysql");
+
+function connection(){
+    return mysql.createConnection({
+      host: "blog.ccsxo0vdgqdw.us-east-1.rds.amazonaws.com",
+      user: "admin",
+      password: "Aa12345678,.",
+      database: "blog",
+    });
+}
+module.exports = connection;
